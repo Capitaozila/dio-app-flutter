abe2c6fe692c3c645edd7f9d90a1b1cb311b35de
Capitaozila <estudanteluis3030@gmail.com>
2023-10-04 17:47:02 -0300

    concluindo a atividade

---
+++ trilhaapp/lib/pages/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:trilhaapp/services/gerador_numero_aleatorio_service.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var numeroGerado = 0;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meu app'),
+      ),
+      body: Center(child: Text(numeroGerado.toString())),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            numeroGerado =
+            GeradorNumeroAleatorioService.gerarNumeroAleatorio(1000);
+            debugPrint(numeroGerado.toString());
+          });
+        },
+      ),
+    );
+  }
+}
